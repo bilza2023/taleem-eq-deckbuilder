1ed55a3c22d8081c38aef5841de80a179b4fe86b
bilza2023 <bilza2023@gmail.com>
2025-06-08 18:00:55 +0500

    zero

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+import { DeckBuilder } from "./src/TwoTemplates/DeckBuilder";
+import {GlobalThemes} from "./src/theme/globalThemes";
+import {GlobalBackgrounds} from "./src/theme/globalBackgrounds";
+ 
+export{
+    DeckBuilder,
+    GlobalThemes,
+    GlobalBackgrounds
+}
