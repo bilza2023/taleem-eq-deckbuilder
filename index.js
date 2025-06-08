eb13660bcdef13df1d4057495829d55668b7d71f
bilza2023 <bilza2023@gmail.com>
2025-06-08 18:26:38 +0500

    removed demo file

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 
 import { EqDeckBuilder } from "./src/EqDeckBuilder";
 import { Sp } from "./src/Sp";
-export { default as spDemo } from './examples/sp-demo.js';
+// export { default as spDemo } from './examples/sp-demo.js';
 
 export{
     EqDeckBuilder,
-    Sp,
-    spDemo
+    Sp
 }
